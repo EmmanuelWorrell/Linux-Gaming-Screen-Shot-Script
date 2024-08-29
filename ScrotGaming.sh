4c75fa5eee1f6fa59dd1dcee7917499bbe192b4c
EmmanuelWorrell <41017916+EmmanuelWorrell@users.noreply.github.com>
2024-08-28 20:52:23 -0400

    Update ScrotGaming.sh
    Fixed an error that cause the script not to move all screenshots to their corresponding folders leaving them in the ~/Pictures/Screenshots directory.

--- ScrotGaming.sh
+++ ScrotGaming.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-outdir="$HOME/Pictures/Screenshots";
-mkdir -p "$outdir" 2>/dev/null;
-appname="$(DISPLAY=:0 xdotool getactivewindow getwindowname 2>/dev/null)";
-appname="${appname:0:32}" # this line limits the appname string to 32 characters
-scrot --silent -c "${outdir}/${appname}-$(date +'%Y-%m-%d--%H%M%S').png";
-mkdir -p "${outdir}/${appname}" 2>/dev/null;
-mv "${outdir}/${appname}-$(date +'%Y-%m-%d--%H%M%S').png" "${outdir}/${appname}/${appname}-$(date +'%Y-%m-%d--%H%M%S').png"
+outdir="$HOME/Pictures/Screenshots"
+mkdir -p "$outdir" 2>/dev/null
+
+# Get the active window's name
+appname="$(DISPLAY=:0 xdotool getactivewindow getwindowname 2>/dev/null)"
+appname="${appname:0:32}" # Limit the appname string to 32 characters
+
+# Take a screenshot
+scrot --silent -c "${outdir}/${appname}-$(date +'%Y-%m-%d--%H%M%S').png"
+
+# Create a subdirectory for the app if it doesn't exist
+mkdir -p "${outdir}/${appname}" 2>/dev/null
+
+# Move all screenshots to the app's subdirectory
+for file in "${outdir}"/*.png; do
+    if [[ "$file" == "${outdir}/${appname}-"* ]]; then
+        mv "$file" "${outdir}/${appname}/"
+    fi
+done
 
